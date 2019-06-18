55156622bc2848b34053a4734bd8ed2fbf6efbce
Alyssa Jane <xcthaeh@gmail.com>
2019-06-18 16:46:03 -0400

    got first dash test working for strike button

--- baseball/src/components/dashboard/Dash.test.js
+++ baseball/src/components/dashboard/Dash.test.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import "jest-dom/extend-expect";
 import Dash from "./Dash";
 
+afterEach(cleanup);
+
 it("renders without crashing", () => {
   render(<Dash />);
+});
+
+describe("The Dash component", () => {
+  it("contains a strike button", () => {
+    const component = render(<Dash />);
+    const button = component.getByTestId("Strike");
+    expect(button).toHaveTextContent(/Strike/i);
+  });
 });
