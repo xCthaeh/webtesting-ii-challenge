b43a6687351fea002f7cc706b77bc3f4c125b43a
Alyssa Jane <xcthaeh@gmail.com>
2019-06-18 17:06:17 -0400

    fixed accidental nesting issue, created last test for fouls, all tests passing

--- baseball/src/components/display/Display.test.js
+++ baseball/src/components/display/Display.test.js
@@ -10,15 +10,23 @@ it("renders without crashing", () => {
 });
 
 describe("The Display component", () => {
+  
   it("displays balls", () => {
     const component = render(<Display strikes={2} balls={1} fouls={2} />);
     const button = component.getByTestId("Balls");
     expect(button).toHaveTextContent(/1 Balls/i);
-
-    it("displays strikes", () => {
-      const component = render(<Display strikes={2} balls={1} fouls={2} />);
-      const button = component.getByTestId("Strikes");
-      expect(button).toHaveTextContent(/2 Strikes/i);
-    });
   });
+
+  it("displays strikes", () => {
+    const component = render(<Display strikes={2} balls={1} fouls={2} />);
+    const button = component.getByTestId("Strikes");
+    expect(button).toHaveTextContent(/2 Strikes/i);
+  });
+
+  it("displays fouls", () => {
+    const component = render(<Display strikes={2} balls={1} fouls={2} />);
+    const button = component.getByTestId("Fouls");
+    expect(button).toHaveTextContent(/2 Fouls/i);
+  });
+
 });
