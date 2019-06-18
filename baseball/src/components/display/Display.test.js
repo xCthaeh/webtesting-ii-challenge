1e691357cade2a850cbfbe4c0a942ea4a793e80f
Alyssa Jane <xcthaeh@gmail.com>
2019-06-18 17:00:20 -0400

    created first display test for balls, working

--- baseball/src/components/display/Display.test.js
+++ baseball/src/components/display/Display.test.js
@@ -3,6 +3,16 @@ import { render, cleanup } from "@testing-library/react";
 import "jest-dom/extend-expect";
 import Display from "./Display";
 
+afterEach(cleanup);
+
 it("renders without crashing", () => {
   render(<Display />);
+});
+
+describe("The Display component", () => {
+  it("displays balls", () => {
+    const component = render(<Display strikes={2} balls={1} fouls={2} />);
+    const button = component.getByTestId("Balls");
+    expect(button).toHaveTextContent(/1 Balls/i);
+  });
 });
