b9d9f8b654a0398b68e5bcf4b3c577cda46d83ef
Alyssa Jane <xcthaeh@gmail.com>
2019-06-18 17:36:35 -0400

    created first test for strikes in app.test, passing

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 import App from "./App";
+import "jest-dom/extend-expect";
 
+afterEach(cleanup);
 
 it("renders without crashing", () => {
   render(<App />);
+});
+
+describe("The baseball app", () => {
+  describe("button functionality", () => {
+    it("has a strike button that logs strikes", () => {
+      const { getByText, getByTestId } = render(<App />);
+
+      const button = getByText("Strike");
+
+      fireEvent.click(button);
+      const strikes = getByTestId("Strikes");
+
+      expect(strikes).toHaveTextContent(/1 Strikes/i);
+    });
+  });
 });
